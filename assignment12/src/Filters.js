504694fd5c7fb289405c0962f73e26e340457c04
Ronak Thakkar <rthakkar5873@sdsu.edu>
2020-11-29 23:36:43 -0800

    Final Changes
    Final Changes

---
+++ assignment12/src/Filters.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+class Filters extends Component {
+constructor(props) {
+     super(props);
+     this.handleChange = this.handleChange.bind(this);
+}
+handleChange(e) {
+     const value = e.target.value
+     const name = e.target.name
+
+     this.props.onFilter({
+          [name]: value
+     });
+}
+	render() {
+		return (
+			<input type="text" name="filterText" placeholder="Search..." className="w-100 form-control my-3" onChange={this.handleChange} autoComplete="off" />
+		)
+	}
+
+}
+export default Filters;
---
+++ assignment12/src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom';
+import Products from './Product';
+
+ReactDOM.render(<Products />, document.getElementById('root'));
